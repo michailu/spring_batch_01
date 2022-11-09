cca7875fff0ec620f7eee416a6cf4b9dcf38cf6c
michailu <luciano.michailu@dataprev.gov.br>
2022-11-09 11:57:55 -0300

    So comentarios
    So comentarios

--- src/main/java/br/com/michailu/spring_batch_01/SpringBatch01Application.java
+++ src/main/java/br/com/michailu/spring_batch_01/SpringBatch01Application.java
@@ -3,6 +3,12 @@ package br.com.michailu.spring_batch_01;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * application.properties externalizado
+ * dois bancos de dados configurados, um para o spring batch e outro para aplicacao
+ * utiliza log4j
+ * disponivel no github
+ */
 @SpringBootApplication
 public class SpringBatch01Application {
 
