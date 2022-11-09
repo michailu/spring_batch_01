7a6bccd71a076ff7e0d11a0d00ef72c2b0e1f80a
luciano.michailu <luciano.michailu@dataprev.gov.br>
2022-11-09 07:29:14 -0300

    Versao inicial

---
+++ src/main/java/br/com/michailu/spring_batch_01/SpringBatch01Application.java
@@ -0,0 +1,13 @@
+package br.com.michailu.spring_batch_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatch01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatch01Application.class, args);
+	}
+
+}
