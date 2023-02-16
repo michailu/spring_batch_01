a6b56b443cb73a0f70bb04e44cbe9587e49edb18
michailu <luciano.michailu@dataprev.gov.br>
2023-02-16 12:19:51 -0300

    Formatacao
    Formatacao

--- src/main/java/br/com/michailu/spring_batch_01/tasklet/LimparTabelaAuxiliosTasklet.java
+++ src/main/java/br/com/michailu/spring_batch_01/tasklet/LimparTabelaAuxiliosTasklet.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class LimparTabelaAuxiliosTasklet extends JdbcDaoSupport implements Tasklet {
-	
+
 	private static final Logger LOGGER = LogManager.getLogger(LimparTabelaAuxiliosTasklet.class);
 
 	@Autowired
